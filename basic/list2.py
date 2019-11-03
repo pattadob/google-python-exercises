809f74a1fc69b65328c2d38b9d65b7d6a7261c7b
pattadob <pattadon.b@gmail.com>
2019-11-03 11:12:29 +0700

    list2.py remove_adjacent done

--- basic/list2.py
+++ basic/list2.py
@@ -14,8 +14,14 @@
 # modify the passed in list.
 def remove_adjacent(nums):
   # +++your code here+++
-  return
+  return remove_adjacent_new_list(nums)
 
+def remove_adjacent_new_list(nums):
+  list_new = []
+  for n in nums:
+    if list_new.count(n) == 0:
+      list_new.append(n)
+  return list_new
 
 # E. Given two lists sorted in increasing order, create and return a merged
 # list of all the elements in sorted order. You may modify the passed in lists.
@@ -41,18 +47,18 @@ def test(got, expected):
     prefix = ' OK '
   else:
     prefix = '  X '
-  print '%s got: %s expected: %s' % (prefix, repr(got), repr(expected))
+  print('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
 
 
 # Calls the above functions with interesting inputs.
 def main():
-  print 'remove_adjacent'
+  print('remove_adjacent')
   test(remove_adjacent([1, 2, 2, 3]), [1, 2, 3])
   test(remove_adjacent([2, 2, 3, 3, 3]), [2, 3])
   test(remove_adjacent([]), [])
 
   print
-  print 'linear_merge'
+  print('linear_merge')
   test(linear_merge(['aa', 'xx', 'zz'], ['bb', 'cc']),
        ['aa', 'bb', 'cc', 'xx', 'zz'])
   test(linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz']),
