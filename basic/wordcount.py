bfa53b454e77832e172cbc4ee9465ea6e2fab9a9
pattadob <pattadon.b@gmail.com>
2019-11-05 20:04:05 +0700

    wordcount.py option --count done

--- basic/wordcount.py
+++ basic/wordcount.py
@@ -44,14 +44,28 @@ import sys
 # You could write a helper utility function that reads a file
 # and builds and returns a word/count dict for it.
 # Then print_words() and print_top() can just call the utility function.
-
+def print_words(filename :str):
+  print('words count ' + filename)
+  dict = {}
+  f = open(filename, 'r')
+  for line in f:
+    # print(line.split())
+    for word in line.split():
+      if word in dict:
+        current = dict[word]
+        dict[word] = current + 1
+      else:
+        dict[word] = 1
+  f.close()
+  for key in sorted(dict.keys(), key=str.lower):
+    print(key, dict[key])
 ###
 
 # This basic command line argument parsing code is provided and
 # calls the print_words() and print_top() functions which you must define.
 def main():
   if len(sys.argv) != 3:
-    print 'usage: ./wordcount.py {--count | --topcount} file'
+    print('usage: ./wordcount.py {--count | --topcount} file')
     sys.exit(1)
 
   option = sys.argv[1]
@@ -59,9 +73,10 @@ def main():
   if option == '--count':
     print_words(filename)
   elif option == '--topcount':
-    print_top(filename)
+    print('print_top()')
+    # print_top(filename)
   else:
-    print 'unknown option: ' + option
+    print('unknown option: ' + option)
     sys.exit(1)
 
 if __name__ == '__main__':
